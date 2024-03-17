159a647e39ab114a1896f2ecb98340bf98461063
swapnilbandbe811 <55969037+swapnilbandbe811@users.noreply.github.com>
2024-03-17 06:02:06 +0000

    operation

--- js_basics/03_conversionOperation.js
+++ js_basics/03_conversionOperation.js
@@ -1,47 +1,70 @@
 // Convert to Number
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)  // convert to number 
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)  // convert to number 
+// console.log(typeof valueInNumber);
 
-let score1 = "33ab"
-let valueInNumber1 = Number(score1)  // convert to number 
-console.log(valueInNumber1,typeof valueInNumber1); // NaN (not a number), Number
+// let score1 = "33ab"
+// let valueInNumber1 = Number(score1)  // convert to number 
+// console.log(valueInNumber1,typeof valueInNumber1); // NaN (not a number), Number
 
-// "33" => 33 conversion
-// "33abc" => Nan
-// true => 1, false => 0
-// undefined => nan
+// // "33" => 33 conversion
+// // "33abc" => Nan
+// // true => 1, false => 0
+// // undefined => nan
 
-// Convert to boolean
+// // Convert to boolean
 
-let isLogged =1
+// let isLogged =1
 
-let booleanIsLogged = Boolean(isLogged)
-console.log(booleanIsLogged); // true 
+// let booleanIsLogged = Boolean(isLogged)
+// console.log(booleanIsLogged); // true 
 
-let isLogged1 = ""
+// let isLogged1 = ""
 
-let booleanIsLogged1 = Boolean(isLogged1)
-console.log(booleanIsLogged1); // false
+// let booleanIsLogged1 = Boolean(isLogged1)
+// console.log(booleanIsLogged1); // false
 
-let isLogged2 = "swapnil"
+// let isLogged2 = "swapnil"
 
-let booleanIsLogged2 = Boolean(isLogged2)
-console.log(booleanIsLogged2); // true
+// let booleanIsLogged2 = Boolean(isLogged2)
+// console.log(booleanIsLogged2); // true
 
-//convert to String
+// //convert to String
 
-let someNumber = 33
+// let someNumber = 33
 
-let strngNumber = String(someNumber)
-console.log(strngNumber, typeof strngNumber); // 33 string
+// let strngNumber = String(someNumber)
+// console.log(strngNumber, typeof strngNumber); // 33 string
 
-let someNumber1 = 45.8678
+// let someNumber1 = 45.8678
 
-let strngNumber1 = String(someNumber1)
-console.log(strngNumber1, typeof strngNumber1); // 
+// let strngNumber1 = String(someNumber1)
+// console.log(strngNumber1, typeof strngNumber1); // 
 
+// ************************************* OPERATIONS ****************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue); // -3
+
+console.log(2+2); //4 
+console.log(2**3); // power 
+console.log(2%3); // remainder
+
+let str1 = "hello"
+
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter);
